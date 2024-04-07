a3e94dfb7a646ac3f6d9af7e2bedb6efe42abed8
macleankatherine <156031022+macleankatherine@users.noreply.github.com>
2024-04-07 18:05:37 -0400

    Login/Register
    implements logging in, registering, and printing users.

---
+++ DDL.sql
@@ -0,0 +1,127 @@
+CREATE TABLE IF NOT EXISTS Members(
+member_id SERIAL PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE, -- Add email field
+password VARCHAR(255) NOT NULL, -- Add password field
+phone_number VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS exersize_routines(
+routine_id SERIAL PRIMARY KEY,
+member_id INT REFERENCES Members(member_id),
+goal_name VARCHAR(255) NOT NULL,
+goal_desciption VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Fitness_Goals(
+goal_id SERIAL PRIMARY KEY,
+member_id INT REFERENCES Members(member_id),
+goal_name VARCHAR(255) NOT NULL,
+goal_desciption VARCHAR(255) NOT NULL
+);
+
+-- we should change health metrics to contrete attributes to choose from IMO
+CREATE TABLE IF NOT EXISTS health_Metrics(
+metric_id SERIAL PRIMARY KEY,
+member_metric_id INT REFERENCES Members(member_id),
+goal_name VARCHAR(255) NOT NULL,
+goal_desciption VARCHAR(255) NOT NULL
+);
+
+-- skipping commits To -> exersize Routines
+
+
+CREATE TABLE IF NOT EXISTS Trainers(
+trainer_id SERIAL PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Trainers(
+admin_id SERIAL PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Trainers(
+admin_id SERIAL PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Administrators(
+admin_id SERIAL PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS personal_training_classes(
+session_id SERIAL PRIMARY KEY,
+trainer_id INT REFERENCES Trainers(trainer_id),
+member_id INT REFERENCES Members(member_id)
+);
+
+CREATE TABLE IF NOT EXISTS group_training_classes(
+class_id SERIAL PRIMARY KEY,
+trainer_id INT REFERENCES Trainers(trainer_id),
+member_id INT REFERENCES Members(member_id),
+theDay DATE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Equipment(
+equipment_id SERIAL PRIMARY KEY,
+equipment_name VARCHAR(255) NOT NULL,
+equipment_description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Equipment_Maintentence(
+class_id SERIAL PRIMARY KEY,
+responsible_admin_id INT REFERENCES Administrators(admin_id),
+equipment_name VARCHAR(255) NOT NULL,
+equipment_id INT REFERENCES Equipment(equipment_id),
+theDay DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS billing(
+billing_id SERIAL PRIMARY KEY,
+status BOOLEAN NOT NULL,
+amount FLOAT NOT NULL,
+billing_session_id INT REFERENCES personal_training_classes(session_id),
+member_id INT REFERENCES Members(member_id)
+);
+
+
+
+-- -- everything under this line is testing
+
+-- -- reset table data
+-- TRUNCATE  Fitness_Goals CASCADE;
+
+-- -- Insert test data into Members table
+-- INSERT INTO Members (first_name, last_name, username, phone_number)
+-- VALUES 
+--     ('John', 'Doe', 'johndoe', '123-456-7890'),
+--     ('Jane', 'Smith', 'janesmith', '987-654-3210');
+
+-- -- Insert test data into Fitness_Goals table
+-- INSERT INTO Fitness_Goals (member_id, goal_name, goal_desciption)
+-- VALUES 
+--     (1, 'Weight Loss', 'Lose 10 pounds in 2 months'),
+--     (1, 'Muscle Gain', 'Gain muscle mass and increase strength');
+
+
+-- SELECT *
+-- FROM Members 
+-- INNER JOIN Fitness_Goals ON Fitness_Goals.member_id = Members.member_id;
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # COMP_3005_Final_Py
  Final Project using Python
+
+  python -m pip install psycopg2-binary
+
+To run:
+python register.py
