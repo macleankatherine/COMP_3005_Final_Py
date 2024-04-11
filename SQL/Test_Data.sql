52d5c41d3dbc24299dc9e078c4ac86d818403d89
macleankatherine <156031022+macleankatherine@users.noreply.github.com>
2024-04-11 17:38:00 -0400

    DDL Update
    switched from TIMESTAMPS to simple variables

--- SQL/Test_Data.sql
+++ SQL/Test_Data.sql
@@ -17,11 +17,13 @@ VALUES
     ('Alice', 'Smith', 'securepass', '987-654-3210'),
     ('Mike', 'Johnson', 'mypassword', '555-555-5555');
 
-INSERT INTO Room_Bookings (room_id, start_datetime, end_datetime, recurrence)
+
+INSERT INTO Room_Bookings (room_id, day_of_week, start_time, end_time, recurrence)
 VALUES
-    (1, '2024-04-15 09:00:00', '2024-04-15 10:00:00', 'Weekly'),
-    (2, '2024-04-16 14:00:00', '2024-04-16 15:00:00', 'Bi-weekly'),
-    (3, '2024-04-17 10:30:00', '2024-04-17 11:30:00', 'Monthly');
+    (1, 'Monday', '09:00:00', '10:00:00', 'Weekly'),
+    (2, 'Tuesday', '14:00:00', '15:00:00', 'Bi-weekly'),
+    (3, 'Wednesday', '10:30:00', '11:30:00', 'Monthly');
+
 
 INSERT INTO Group_training_classes (trainer_id, name, booking_id, details)
 VALUES
@@ -34,3 +36,14 @@ VALUES
     (1, 1), -- Yoga Class with member_id 1
     (2, 2), -- HIIT Workout with member_id 2
     (3, 3); -- Zumba Dance Party with member_id 3
+
+INSERT INTO Trainer_Availability (trainer_id, day_of_week, start_time, end_time) VALUES
+    (1, 'Monday', '08:00', '22:00'),
+    (1, 'Tuesday', '08:00', '22:00'),
+    (1, 'Wednesday', '08:00', '22:00'),
+    (1, 'Thursday', '08:00', '22:00'),
+    (1, 'Friday', '08:00', '22:00'),
+    (1, 'Saturday', '08:00', '22:00'),
+    (1, 'Sunday', '08:00', '22:00'),
+    (2, 'Tuesday', '13:00', '17:00'),
+    (3, 'Friday', '11:00', '15:00');
